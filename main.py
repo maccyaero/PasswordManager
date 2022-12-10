ca16f9332f7ca76ee4b30111b15bb33085ad3666
maccyaero <17176445+maccyaero@users.noreply.github.com>
2022-12-10 14:39:37 +1100

    Added code to empty all Entry fields after submitting to get them ready for the next input

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 import tkinter
+import pandas as pd
+
+website_name = " "
+email = " "
+password = " "
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_password():
+    global website_name
+    global email
+    global password
+    website_name = website_input.get()
+    email = email_input.get()
+    password = password_input.get()
+    d = {'Website': [website_name], 'Username': [email], 'Password': [password]}
+    df = pd.DataFrame(data=d)
+    print(df)
+    df.to_csv('out.csv', index=False, mode='a', header=False)
+    # Empty all Entry fields after submitting to get them ready for the next input
+    website_input.delete(0, len(website_name))
+    email_input.delete(0, len(email_input))
+    password_input.delete(0, len(password))
+
+    # Find a way to stop header from repeating and show only once
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -20,29 +43,30 @@ canvas.grid(column=1, row=0)
 Website_label = tkinter.Label(text="Website", )
 Website_label.grid(column=0, row=1, )
 # Website Input
-website_input = tkinter.Entry( width=36)
+website_input = tkinter.Entry(width=36)
 website_input.grid(column=1, row=1, columnspan=2)
+website_input.focus()
 
 # Email Label
 Email_label = tkinter.Label(text="Email", )
 Email_label.grid(column=0, row=2)
 
 # Email Input
-email_input = tkinter.Entry( width=36)
+email_input = tkinter.Entry(width=36)
 email_input.grid(column=1, row=2, columnspan=2)
 
 # Password Label
 Password_label = tkinter.Label(text="Password", )
 Password_label.grid(column=0, row=3)
 # Password Input
-password_input = tkinter.Entry( width=19)
+password_input = tkinter.Entry(width=19)
 password_input.grid(column=1, row=3)
 
 # Generate Password Button
-Password_button = tkinter.Button(text="Generate Password", highlightthickness=0,)
+Password_button = tkinter.Button(text="Generate Password", highlightthickness=0, )
 Password_button.grid(column=2, row=3)
 # Add Button
-Add_button = tkinter.Button(text="Add", highlightthickness=0, width=36)
-Add_button.grid(column=1, row=4, columnspan=2)
+Add_button = tkinter.Button(text="Add", highlightthickness=0, width=36, command=save_password)
+Add_button.grid(column=1, row=4, columnspan=2, )
 
 window.mainloop()
